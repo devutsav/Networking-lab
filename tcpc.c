5ffc0df5f842cd730393f6ad8dd5b37a9a71fc8d
Utsav Das <das.utsav1995@gmail.com>
2017-05-07 02:31:18 +0530

    Update tcpc.c

--- tcpc.c
+++ tcpc.c
@@ -22,7 +22,7 @@ int main()
 		bzero(&serv_addr,sizeof(serv_addr));
 
 	serv_addr.sin_family=AF_INET;
-	serv_addr.sin_addr.s_addr=inet_addr("127.0.0.1");
+	serv_addr.sin_addr.s_addr=INADDR_ANY;
 	serv_addr.sin_port=htons(SERVER);
 
 
